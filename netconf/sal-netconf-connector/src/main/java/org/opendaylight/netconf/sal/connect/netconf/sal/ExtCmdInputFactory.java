4032fef4e128b1a3f9c25981d08dc7aaa1070fcf
Huafei Zhang <huafei.zhang@utstar.com>
2019-11-22 10:33:35 +0800

    ENHANCE: 南向分页和查询总数接口的实现

---
+++ netconf/sal-netconf-connector/src/main/java/org/opendaylight/netconf/sal/connect/netconf/sal/ExtCmdInputFactory.java
@@ -0,0 +1,79 @@
+package org.opendaylight.netconf.sal.connect.netconf.sal;
+
+import com.google.common.collect.Maps;
+import org.opendaylight.netconf.api.xml.XmlUtil;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.AnyXmlNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.api.NormalizedNodeAttrBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import javax.xml.transform.dom.DOMSource;
+import java.util.Map;
+
+public class ExtCmdInputFactory {
+    public static final QName UTSTARCOM_EXT =
+            QName.create("urn.utstar:uar:ExtCmd", "2017-11-02", "utstarcom-ext").intern();
+
+    public static final QName COMMAND = QName.create(UTSTARCOM_EXT, "Command");
+    public static final QName EXT_CMD_RPC_QNAME = QName.create(UTSTARCOM_EXT, "extcmd");
+
+    public static AnyXmlNode createExtCmdInput(String tableName, String type) {
+        final NormalizedNodeAttrBuilder<YangInstanceIdentifier.NodeIdentifier, DOMSource, AnyXmlNode> anyXmlBuilder =
+                Builders.anyXmlBuilder().withNodeIdentifier(new YangInstanceIdentifier.NodeIdentifier(
+                        QName.create(UTSTARCOM_EXT, COMMAND.getLocalName()).intern()));
+        Map<QName, String> attributes = Maps.newHashMap();
+        anyXmlBuilder.withAttributes(attributes);
+
+        Document doc = XmlUtil.newDocument();
+        final Element element =
+                doc.createElementNS(COMMAND.getNamespace().toString(), COMMAND.getLocalName());
+
+        element.appendChild(createExtCmds(doc, tableName, type));
+        anyXmlBuilder.withValue(new DOMSource(element));
+        return anyXmlBuilder.build();
+    }
+
+    private static Node createExtCmds(Document doc, String tableName, String type) {
+        Element extCmds = doc.createElement("ExtCmds");
+        extCmds.appendChild(createExtCmd(doc, tableName, type));
+        return extCmds;
+    }
+
+    private static Node createExtCmd(Document doc, String tableName, String type) {
+        Element extCmd = doc.createElement("ExtCmd");
+
+        Element index = doc.createElement("Index");
+        index.appendChild(doc.createTextNode("1"));
+
+        Element cmdName = doc.createElement("CmdName");
+        cmdName.appendChild(doc.createTextNode("queryCnt"));
+
+        Element operation = doc.createElement("Operation");
+        operation.appendChild(doc.createTextNode("execute"));
+
+        Element timeout = doc.createElement("Timeout");
+        timeout.appendChild(doc.createTextNode("10"));
+
+        Element sync = doc.createElement("Sync");
+        sync.appendChild(doc.createTextNode("1"));
+
+        String paraValue = String.format("{{\"DsName\",{String,\"%s\"}},{\"TblName\",{String,\"%s\"}}}", type, tableName);
+        Element params = doc.createElement("Params");
+        params.appendChild(doc.createTextNode(paraValue));
+
+        extCmd.appendChild(index);
+        extCmd.appendChild(cmdName);
+        extCmd.appendChild(operation);
+        extCmd.appendChild(timeout);
+        extCmd.appendChild(sync);
+        extCmd.appendChild(params);
+
+        return extCmd;
+    }
+
+}
