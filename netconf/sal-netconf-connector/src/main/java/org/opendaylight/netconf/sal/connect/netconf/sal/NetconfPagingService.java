3b447c8706f0a97be3c4fcc89d1c11a9cf44aaab
Huafei Zhang <huafei.zhang@utstar.com>
2019-11-25 14:27:55 +0800

    TEMP 分离BI和BA返回
    TEMP: 调整异常处理和代码逻辑
    
    TEMP: 调整分页接口参数，接口代码

--- netconf/sal-netconf-connector/src/main/java/org/opendaylight/netconf/sal/connect/netconf/sal/NetconfPagingService.java
+++ netconf/sal-netconf-connector/src/main/java/org/opendaylight/netconf/sal/connect/netconf/sal/NetconfPagingService.java
@@ -8,31 +8,80 @@
 package org.opendaylight.netconf.sal.connect.netconf.sal;
 
 import com.google.common.base.Optional;
+import com.google.common.util.concurrent.FluentFuture;
 import com.google.common.util.concurrent.ListenableFuture;
 import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 
-/***
+import javax.annotation.Nullable;
+
+/**
  * 分页服务
+ *
+ * @author Huafei Zhang
  */
 public interface NetconfPagingService {
 
+    // 常量特定于yang的生成规则
+    static final String NAMESPACE_PREFIX = "urn.utstar:uar:";
+    static final String REVISION_DATE = "2017-11-02";
+    static final String TOP_CONTAINER_SUFFIX = "s";
+
+    static <T extends DataObject> YangInstanceIdentifier toTableYangII(String moduleName) {
+        QName topContainer = topoContainerQname(moduleName);
+        return YangInstanceIdentifier.builder().node(topContainer).build();
+    }
+
+    static QName topoContainerQname(String moduleName) {
+        QName moduleQname = QName.create(NAMESPACE_PREFIX + moduleName, REVISION_DATE, moduleName).intern();
+        QName topContainer = QName.create(moduleQname, topContainerName(moduleName));
+        return topContainer;
+    }
+
+    static String topContainerName(String moduleName) {
+        return moduleName + TOP_CONTAINER_SUFFIX;
+    }
+
+    static String namespace(String moduleName) {
+        return NAMESPACE_PREFIX + moduleName;
+    }
+
     /**
-     *
-     * @param nodeId 网元ID
+     * @param nodeId    网元ID
      * @param tableName 表名
-     * @param type cfg or stat
+     * @param type      cfg or stat
      * @return 表的总条数
      */
-     ListenableFuture<Integer> queryCount(String nodeId, String tableName, String type) ;
+    ListenableFuture<Integer> queryCount(String nodeId, String tableName, TableType type);
 
     /***
      *
      * @param nodeId 网元ID
-     * @param topContainer yang顶层container生成的class
-     * @param base 分页起点
-     * @param num 分页末点
+     * @param moduleName yang模型名字
+     * @param start 分页起点
+     * @param num 数目
      * @param expressions 查询的条件表达式
      * @return 查询到的Binding类型的数据
      */
-     <T extends DataObject> ListenableFuture<Optional<T>> find(String nodeId, final Class<T> topContainer, Integer base, Integer num, String... expressions);
+    <T extends DataObject> FluentFuture<Optional<T>> query(String nodeId, final String moduleName, @Nullable Integer start, @Nullable Integer num, @Nullable String... expressions);
+
+
+    /***
+     *
+     * @param nodeId 网元ID
+     * @param moduleName yang模型名字
+     * @param start 分页起点
+     * @param num 数目
+     * @param expressions 查询的条件表达式
+     * @return 查询到的Binding类型的数据
+     */
+    <T extends DataObject> FluentFuture<Optional<NormalizedNode<?, ?>>> find(String nodeId, String moduleName,
+                                                                             @Nullable Integer start, @Nullable Integer num, @Nullable String... expressions);
+
+    enum TableType {
+        cfg, stat;
+    }
+
 }
--- netconf/sal-netconf-connector/src/main/java/org/opendaylight/netconf/sal/connect/netconf/sal/NetconfPagingServiceImpl.java
+++ netconf/sal-netconf-connector/src/main/java/org/opendaylight/netconf/sal/connect/netconf/sal/NetconfPagingServiceImpl.java
@@ -17,7 +17,10 @@ import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.google.common.util.concurrent.SettableFuture;
 import org.opendaylight.mdsal.binding.dom.codec.api.BindingNormalizedNodeSerializer;
-import org.opendaylight.mdsal.dom.api.*;
+import org.opendaylight.mdsal.dom.api.DOMMountPoint;
+import org.opendaylight.mdsal.dom.api.DOMMountPointService;
+import org.opendaylight.mdsal.dom.api.DOMRpcResult;
+import org.opendaylight.mdsal.dom.api.DOMRpcService;
 import org.opendaylight.netconf.api.xml.XmlUtil;
 import org.opendaylight.netconf.sal.connect.netconf.util.NetconfBaseOps;
 import org.opendaylight.netconf.sal.connect.netconf.util.NetconfMessageTransformUtil;
@@ -36,8 +39,9 @@ import org.w3c.dom.Element;
 
 import javax.annotation.Nullable;
 import javax.xml.transform.dom.DOMSource;
-import java.lang.reflect.Field;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.opendaylight.netconf.sal.connect.netconf.sal.ExtCmdInputFactory.EXT_CMD_RPC_QNAME;
 import static org.opendaylight.netconf.sal.connect.netconf.sal.ExtCmdInputFactory.UTSTARCOM_EXT;
@@ -46,16 +50,17 @@ import static org.opendaylight.netconf.sal.connect.netconf.util.NetconfMessageTr
 /**
  * Netconf分页功能
  * <p>
- * Author: Huafei Zhang
+ * @Author Huafei Zhang
  */
 public class NetconfPagingServiceImpl implements NetconfPagingService {
 
     private DOMMountPointService domMountService;
     final private BindingNormalizedNodeSerializer codec;
-    private	static DOMSchemaService domService;
 
     private static final String DEFAULT_TOPOLOGY_NAME = "topology-netconf";
-    private static final String  CONDITION_REGEX = "\\w+[>,<,=]{1}'.*'";
+    private static final String CONDITION_REGEX = "(\\w+)([>,<,=]{1})(.*)";
+
+
     private static final YangInstanceIdentifier DEFAULT_TOPOLOGY_NODE =
             YangInstanceIdentifier.builder().node(NetworkTopology.QNAME).node(Topology.QNAME)
                     .nodeWithKey(Topology.QNAME, QName.create(Topology.QNAME, "topology-id"), DEFAULT_TOPOLOGY_NAME)
@@ -64,8 +69,6 @@ public class NetconfPagingServiceImpl implements NetconfPagingService {
     private static final String XPATH = "xpath";
     private static final QName NETCONF_SELECT_QNAME = QName.create(NETCONF_QNAME, "select").intern();
     private static final String NAMESPACE_PREFIX = "t";
-
-
 
     public NetconfPagingServiceImpl(BindingNormalizedNodeSerializer codec, DOMMountPointService domMountService) {
         this.codec = codec;
@@ -77,7 +80,8 @@ public class NetconfPagingServiceImpl implements NetconfPagingService {
                 .nodeWithKey(Node.QNAME, QName.create(Node.QNAME, "node-id"), nodeId).build();
     }
 
-    public ListenableFuture<Integer> queryCount(String nodeId, String tableName, String type) {
+    public ListenableFuture<Integer> queryCount(String nodeId, String moduleName, TableType type) {
+        Preconditions.checkNotNull(type, "Table type should not be null");
         YangInstanceIdentifier nodeII = toYangNodeII(nodeId);
         Optional<DOMMountPoint> mountPointOpt = domMountService.getMountPoint(nodeII);
         if (!mountPointOpt.isPresent()) {
@@ -87,82 +91,91 @@ public class NetconfPagingServiceImpl implements NetconfPagingService {
         }
         DOMRpcService rpcService = mountPointOpt.get().getService(DOMRpcService.class).get();
         SchemaPath rpcType = SchemaPath.create(true, EXT_CMD_RPC_QNAME);
-        AnyXmlNode extCmdInput = ExtCmdInputFactory.createExtCmdInput(tableName, type);
+        AnyXmlNode extCmdInput = ExtCmdInputFactory.createExtCmdInput(moduleName, type);
         FluentFuture<DOMRpcResult> resultFuture = rpcService.invokeRpc(rpcType, NetconfMessageTransformUtil.wrap(EXT_CMD_RPC_QNAME, extCmdInput));
         return resultFuture.transform(domRpcResult -> {
             Preconditions.checkArgument(domRpcResult.getErrors().isEmpty(), "%s: Unable to query count of %s, errors: %s",
-                    nodeId, tableName, domRpcResult.getErrors());
+                    nodeId, moduleName, domRpcResult.getErrors());
             final DataContainerChild<? extends YangInstanceIdentifier.PathArgument, ?> reply =
                     ((ContainerNode) domRpcResult.getResult())
                             .getChild(NetconfMessageTransformUtil.toId(QName.create(UTSTARCOM_EXT, "reply").intern())).get();
 
-            AnyXmlNode replyValue = (AnyXmlNode) reply.getValue();
-            DOMSource domSource = replyValue.getValue();
+            DOMSource domSource = ((AnyXmlNode) reply).getValue();
             Element domReply = (Element) domSource.getNode();
-            String count = domReply.getFirstChild().getTextContent();
-            if(!Strings.isNullOrEmpty(count)) {
-                return Integer.parseInt(count);
+            if(domReply.getFirstChild() == null || Strings.isNullOrEmpty(domReply.getFirstChild().getTextContent())) {
+                return 0;
             }
-            return -1;
+            String count = domReply.getFirstChild().getTextContent();
+            return Integer.parseInt(count);
         }, MoreExecutors.directExecutor());
     }
 
-    public <T extends DataObject> ListenableFuture<Optional<T>> find(String nodeId, final Class<T> topContainer,
-                                                                     @Nullable Integer base, @Nullable Integer num, @Nullable String... expressions) {
-        Preconditions.checkArgument((base == null || base >= 0) && (num == null || num > 0), "limit not illegal: base = %s, num = %s", base, num);
-        for(String exp : expressions) {
-            Preconditions.checkArgument(exp.matches(CONDITION_REGEX), "%s: format not correct", exp);
-        }
-
-        try {
-            YangInstanceIdentifier yangII = toTableYangII(topContainer);
-            YangInstanceIdentifier nodeII = toYangNodeII(nodeId);
-            Optional<DOMMountPoint> mountPointOpt = domMountService.getMountPoint(nodeII);
-            if (!mountPointOpt.isPresent()) {
-                SettableFuture<Optional<T>> future = SettableFuture.create();
-                future.setException(new IllegalStateException("Specified mount point " + nodeId + " not exist"));
-                return future;
+    @Override
+    public <T extends DataObject> FluentFuture<Optional<T>> query(String nodeId, final String moduleName,
+                                                                  @Nullable Integer start, @Nullable Integer num, @Nullable String... expressions) {
+        YangInstanceIdentifier yangII = NetconfPagingService.toTableYangII(moduleName);
+        return find(nodeId, moduleName, start, num, expressions).transform(resutOpt -> {
+            if (!resutOpt.isPresent()) {
+                return Optional.of((T) codec.fromNormalizedNode(yangII, resutOpt.get()).getValue());
             }
+            return Optional.<T>absent();
 
-            DOMRpcService rpcService = mountPointOpt.get().getService(DOMRpcService.class).get();
-            SchemaPath type = SchemaPath.create(true, NETCONF_GET_CONFIG_QNAME);
-            final NormalizedNodeAttrBuilder<YangInstanceIdentifier.NodeIdentifier, DOMSource, AnyXmlNode> anyXmlBuilder =
-                    toFitlerStructure(topContainer, base, num, expressions);
-            DataContainerChild<?, ?> invokeInput = NetconfMessageTransformUtil.wrap(NETCONF_GET_CONFIG_QNAME,
-                    NetconfBaseOps.getSourceNode(NETCONF_RUNNING_QNAME), anyXmlBuilder.build());
+        }, MoreExecutors.directExecutor());
+    }
 
-            FluentFuture<DOMRpcResult> resultFuture = rpcService.invokeRpc(type, invokeInput);
-            return resultFuture.transform(domRpcResult -> {
-                Preconditions.checkArgument(domRpcResult.getErrors().isEmpty(), "Unable to read data: %s, errors: %s",
-                        topContainer.getSimpleName(), domRpcResult.getErrors());
-                final DataContainerChild<? extends YangInstanceIdentifier.PathArgument, ?> dataNode =
-                        ((ContainerNode) domRpcResult.getResult())
-                                .getChild(NetconfMessageTransformUtil.toId(NETCONF_DATA_QNAME)).get();
+    @Override
+    public <T extends DataObject> FluentFuture<Optional<NormalizedNode<?, ?>>> find(String nodeId, String moduleName,
+                                                                                    @Nullable Integer start, @Nullable Integer num, @Nullable String... expressions) {
+        checkArgument(start, num, expressions);
 
-                java.util.Optional<NormalizedNode<?, ?>> normalizedNodeOptional =
-                        NormalizedNodes.findNode(dataNode, yangII.getPathArguments());
-                if (!normalizedNodeOptional.isPresent()) {
-                    return Optional.absent();
-                }
+        YangInstanceIdentifier yangII = NetconfPagingService.toTableYangII(moduleName);
+        YangInstanceIdentifier nodeII = toYangNodeII(nodeId);
+        Optional<DOMMountPoint> mountPointOpt = domMountService.getMountPoint(nodeII);
+        if (!mountPointOpt.isPresent()) {
+            SettableFuture<Optional<NormalizedNode<?, ?>>> future = SettableFuture.create();
+            future.setException(new IllegalStateException("Specified mount point " + nodeId + " not exist"));
+            return FluentFuture.from(future);
+        }
 
-                return Optional.of((T) codec.fromNormalizedNode(yangII, dataNode).getValue());
-            }, MoreExecutors.directExecutor());
-        } catch (NoSuchFieldException | IllegalAccessException | IllegalArgumentException e) {
-            SettableFuture future = SettableFuture.create();
-            future.setException(e);
-            return future;
+        DOMRpcService rpcService = mountPointOpt.get().getService(DOMRpcService.class).get();
+        SchemaPath type = SchemaPath.create(true, NETCONF_GET_CONFIG_QNAME);
+        final NormalizedNodeAttrBuilder<YangInstanceIdentifier.NodeIdentifier, DOMSource, AnyXmlNode> anyXmlBuilder =
+                toFitlerStructure(moduleName, start, num, expressions);
+        DataContainerChild<?, ?> invokeInput = NetconfMessageTransformUtil.wrap(NETCONF_GET_CONFIG_QNAME,
+                NetconfBaseOps.getSourceNode(NETCONF_RUNNING_QNAME), anyXmlBuilder.build());
+
+        FluentFuture<DOMRpcResult> resultFuture = rpcService.invokeRpc(type, invokeInput);
+        return resultFuture.transform(domRpcResult -> {
+            Preconditions.checkArgument(domRpcResult.getErrors().isEmpty(), "Unable to read data: %s, errors: %s",
+                    NetconfPagingService.topContainerName(moduleName), domRpcResult.getErrors());
+            final DataContainerChild<? extends YangInstanceIdentifier.PathArgument, ?> dataNode =
+                    ((ContainerNode) domRpcResult.getResult())
+                            .getChild(NetconfMessageTransformUtil.toId(NETCONF_DATA_QNAME)).get();
+
+            java.util.Optional<NormalizedNode<?, ?>> normalizedNodeOptional =
+                    NormalizedNodes.findNode(dataNode, yangII.getPathArguments());
+
+            return Optional.fromJavaUtil(normalizedNodeOptional);
+
+        }, MoreExecutors.directExecutor());
+    }
+
+    private void checkArgument(@Nullable Integer start, @Nullable Integer num, @Nullable String[] expressions) {
+        Preconditions.checkArgument(((start != null) && (num != null)) || (expressions != null), "at least one condition is specified");
+        if((start != null) && (num != null)) {
+            Preconditions.checkArgument(start >= 0 && num > 0, "start and num should be non-negative");
+        }
+
+        if (expressions != null) {
+            for (String exp : expressions) {
+                Preconditions.checkArgument(exp.matches(CONDITION_REGEX), "%s: format not correct", exp);
+            }
         }
     }
 
-    private <T extends DataObject> YangInstanceIdentifier toTableYangII(Class<T> topContainer)
-            throws NoSuchFieldException, IllegalAccessException {
-        Field field = topContainer.getField("QNAME");
-        QName qname = (QName) field.get(null);
-        return YangInstanceIdentifier.builder().node(qname).build();
-    }
 
     private <T extends DataObject> NormalizedNodeAttrBuilder<YangInstanceIdentifier.NodeIdentifier, DOMSource, AnyXmlNode> toFitlerStructure(
-            Class<T> topContainer, Integer base, Integer num, String... expressions) throws IllegalAccessException, NoSuchFieldException {
+            String moduleName, Integer start, Integer num, String... expressions) {
         final NormalizedNodeAttrBuilder<YangInstanceIdentifier.NodeIdentifier, DOMSource, AnyXmlNode> anyXmlBuilder =
                 Builders.anyXmlBuilder().withNodeIdentifier(new YangInstanceIdentifier.NodeIdentifier(
                         QName.create(NETCONF_QNAME, NETCONF_FILTER_QNAME.getLocalName()).intern()));
@@ -173,48 +186,52 @@ public class NetconfPagingServiceImpl implements NetconfPagingService {
         final Element element =
                 doc.createElementNS(NETCONF_FILTER_QNAME.getNamespace().toString(), NETCONF_FILTER_QNAME.getLocalName());
         element.setAttribute(NETCONF_TYPE_QNAME.getLocalName(), XPATH);
-        element.setAttribute(NETCONF_SELECT_QNAME.getLocalName(), toXpathExp(topContainer.getSimpleName(), base, num, expressions));
+        element.setAttribute(NETCONF_SELECT_QNAME.getLocalName(), toXpathExp(NetconfPagingService.topContainerName(moduleName), start, num, expressions));
 
-        String namespace = extractNamespace(topContainer);
+        String namespace = NetconfPagingService.namespace(moduleName);
         element.setAttribute(XmlUtil.XMLNS_ATTRIBUTE_KEY + ":" + NAMESPACE_PREFIX, namespace);
         anyXmlBuilder.withValue(new DOMSource(element));
         return anyXmlBuilder;
     }
 
-    private <T extends DataObject> String extractNamespace(Class<T> topContainer)
-            throws NoSuchFieldException, IllegalAccessException {
-        Field field = topContainer.getField("QNAME");
-        QName qName = (QName) field.get(null);
-        return qName.getNamespace().toString();
-    }
-
-    /**
-     * Example: L3VPNStaticIpv4RouteCfgs:/t:L3VPNStaticIpv4RouteCfg[/t:id >='0'][/t:id <='200']
-     */
-    private String toXpathExp(String topContainerName, Integer base, Integer num, String... expressions) {
+    private String toXpathExp(String topContainerName, Integer start, Integer num, String... expressions) {
 
         String prefixSlash = "/" + NAMESPACE_PREFIX + ":";
         String listName = topContainerName.substring(0, topContainerName.length() - 1);
-        String basePath = prefixSlash + topContainerName + prefixSlash + listName;
+        String startPath = prefixSlash + topContainerName + prefixSlash + listName;
 
-        StringBuilder stringBuilder = new StringBuilder(basePath);
-        for(String expression : expressions) {
-            String fexp = String.format("[%s]", expressions);
-            stringBuilder.append(fexp);
+        StringBuilder stringBuilder = new StringBuilder(startPath);
+        if (expressions != null) {
+            for (String expression : expressions) {
+                String fexp = String.format("[%s]", quoteExp(expression));
+                stringBuilder.append(fexp);
+            }
         }
 
-        String limit = toLimit(base, num);
-        if(limit != null) {
+        String limit = toLimit(start, num);
+        if (limit != null) {
             stringBuilder.append(limit);
         }
         return stringBuilder.toString();
     }
 
-    private String toLimit(Integer base, Integer num) {
-        if(base ==  null || num == null) {
+    private String quoteExp(String expression) {
+        Pattern pattern = Pattern.compile(CONDITION_REGEX);
+        Matcher matcher = pattern.matcher(expression);
+        if(matcher.matches()) {
+            String key = matcher.group(1);
+            String operator = matcher.group(2);
+            String value = matcher.group(3);
+            return key + operator + "'" + value + "'";
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private String toLimit(Integer start, Integer num) {
+        if (start == null || num == null) {
             return null;
         }
-        String limit = String.format("[LIMIT()-%s+%s]", base, num);
+        String limit = String.format("[LIMIT()-%s+%s]", start, num);
         return limit;
     }
 }
