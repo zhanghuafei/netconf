bf53f2e5cc6a8b66a12f70802f1b2aa90e7fd44e
Huafei Zhang <huafei.zhang@utstar.com>
2019-12-15 20:39:10 +0800

    FEATURE: 事务隔离性增强实现
    - schemacontext的使用选择，修改对于调用来源识别。
    - 事务隔离性增强
    - 事务id追踪、网元锁ID追踪
    - 事务锁状态的JMX监控，支持打印锁队列状态
    - 网元锁冲突，退避随机时间1-5s，参考以太网的CSMA/CD。
    - 修复BUG： commit结束的计数器非线程安全
    - 事务性能监测
    
    TODO:
    - 限速：地铁站策略
    - 等待时间等参数配置化
    - 文档

--- netconf/sal-netconf-connector/src/main/java/org/opendaylight/netconf/sal/connect/netconf/sal/AcrossDeviceDataBrokerImpl.java
+++ netconf/sal-netconf-connector/src/main/java/org/opendaylight/netconf/sal/connect/netconf/sal/AcrossDeviceDataBrokerImpl.java
@@ -10,28 +10,38 @@ package org.opendaylight.netconf.sal.connect.netconf.sal;
 
 import org.opendaylight.controller.md.sal.dom.api.DOMMountPointService;
 import org.opendaylight.mdsal.binding.dom.codec.api.BindingNormalizedNodeSerializer;
+import org.opendaylight.netconf.sal.connect.netconf.sal.isolation.TransactionScheduler;
 import org.opendaylight.netconf.sal.connect.netconf.sal.tx.AcrossDeviceReadWriteTransaction;
 import org.opendaylight.netconf.sal.connect.netconf.sal.tx.AcrossDeviceWriteTransaction;
 import org.opendaylight.netconf.sal.connect.netconf.sal.tx.BindingAcrossDeviceReadWriteTransaction;
 import org.opendaylight.netconf.sal.connect.netconf.sal.tx.BindingAcrossDeviceWriteTransaction;
 
-public class AcrossDeviceDataBrokerImpl implements AcrossDeviceDataBroker {
+public class AcrossDeviceDataBrokerImpl implements AcrossDeviceDataBroker, AutoCloseable {
 
     private BindingNormalizedNodeSerializer codec;
     private DOMMountPointService mountService;
+    private TransactionScheduler transScheduler;
 
     public AcrossDeviceDataBrokerImpl(BindingNormalizedNodeSerializer codec, DOMMountPointService mountService) {
         this.codec = codec;
         this.mountService = mountService;
+        this.transScheduler = TransactionScheduler.create();
+
+        transScheduler.start();
     }
 
     @Override
     public AcrossDeviceWriteTransaction newWriteOnlyTransaction() {
-        return new BindingAcrossDeviceWriteTransaction(codec, mountService);
+        return new BindingAcrossDeviceWriteTransaction(codec, mountService, transScheduler);
     }
 
     @Override
     public AcrossDeviceReadWriteTransaction newReadWriteTransaction() {
-        return new BindingAcrossDeviceReadWriteTransaction(codec, mountService);
+        return new BindingAcrossDeviceReadWriteTransaction(codec, mountService, transScheduler);
+    }
+
+    @Override
+    public void close() throws Exception {
+        transScheduler.close();
     }
 }
---
+++ netconf/sal-netconf-connector/src/main/java/org/opendaylight/netconf/sal/connect/netconf/sal/isolation/DeviceLockStatsMXBean.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2019 UTStarcom, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.sal.connect.netconf.sal.isolation;
+
+import java.util.Set;
+
+public interface DeviceLockStatsMXBean {
+
+    /**
+     * 当前被锁住的所有网元数量
+     */
+    int getTotalLockedDeviceCount();
+
+    /**
+     * 任务（事务）允许在队列中等待的时间
+     */
+    int getTaskWaitTimeout();
+
+    /**
+     * 当前任务（事务）队列的长度
+     */
+    int getTaskQueueSize();
+
+    /**
+     * 锁竞争失败后延迟执行的时间
+     */
+    String getPostponeDelay();
+
+    /**
+     * 因未能竞争到锁而失败的事务数量
+     */
+    int getFailLockTransCount();
+
+    /**
+     * 提交的任务（事务）总数
+     */
+    long getTotalSubmittedTransCount();
+
+    /**
+     * 所有事务推迟执行的总次数
+     */
+    long getTotalPostponeTimes();
+
+    /**
+     * 操作：打印LockPool中的信息
+     */
+    void printDeviceLocks();
+
+    /**
+     * 操作：清除LockPool中的所有锁
+     */
+    void clearAllLocks();
+
+    /**
+     * 操作：打开事务信息的详细打印
+     */
+    void openTransDetailPrint();
+
+    /**
+     * 操作：关闭事务信息的详细打印
+     */
+    void closeTransDetailPrint();
+
+}
